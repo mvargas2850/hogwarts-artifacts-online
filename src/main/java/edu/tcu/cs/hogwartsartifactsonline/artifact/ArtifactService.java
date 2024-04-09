ac138f9564662f772fe9e286dc6d9542d800181f
mvargas2850 <m.vargas@tcu.edu>
2024-04-09 10:14:45 -0500

    Wizard CRUD done! Close #2

--- src/main/java/edu/tcu/cs/hogwartsartifactsonline/artifact/ArtifactService.java
+++ src/main/java/edu/tcu/cs/hogwartsartifactsonline/artifact/ArtifactService.java
@@ -4,6 +4,8 @@ import edu.tcu.cs.hogwartsartifactsonline.artifact.utils.IdWorker;
 import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
 
+import edu.tcu.cs.hogwartsartifactsonline.system.exception.ObjectNotFoundException;
+
 import java.util.List;
 
 @Service
@@ -22,7 +24,7 @@ public class ArtifactService {
     public Artifact findById(String artifactId) {
         //return this.artifactRepository.findById(artifactId).get(); //get because we want artifact from optional (findById) though it doesn't handle exceptions so we use orElseThrow
         return this.artifactRepository.findById(artifactId)
-                .orElseThrow(() -> new ArtifactNotFoundException(artifactId));
+                .orElseThrow(() -> new ObjectNotFoundException("artifact", artifactId));
     }
 
     public List<Artifact> findAll(){
@@ -42,12 +44,12 @@ public class ArtifactService {
                     oldArtifact.setImageUrl(update.getImageUrl());
                     return this.artifactRepository.save(oldArtifact);
                 })
-                .orElseThrow(() -> new ArtifactNotFoundException(artifactId));
+                .orElseThrow(() -> new ObjectNotFoundException("artifact", artifactId));
     }
 
     public void delete(String artifactId){
         this.artifactRepository.findById(artifactId)
-                .orElseThrow(() -> new ArtifactNotFoundException(artifactId));
+                .orElseThrow(() -> new ObjectNotFoundException("artifact", artifactId));
         this.artifactRepository.deleteById(artifactId);
     }
 
---
+++ src/main/java/edu/tcu/cs/hogwartsartifactsonline/wizard/WizardController.java
@@ -0,0 +1,80 @@
+package edu.tcu.cs.hogwartsartifactsonline.wizard;
+
+import edu.tcu.cs.hogwartsartifactsonline.system.Result;
+import edu.tcu.cs.hogwartsartifactsonline.system.StatusCode;
+import edu.tcu.cs.hogwartsartifactsonline.wizard.converter.WizardDtoToWizardConverter;
+import edu.tcu.cs.hogwartsartifactsonline.wizard.converter.WizardToWizardDtoConverter;
+import edu.tcu.cs.hogwartsartifactsonline.wizard.dto.WizardDto;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+//Controllers in Spring Boot are responsible for handling incoming HTTP requests from clients,
+//processing them, and returning an appropriate HTTP response.
+//They act as an entry point for client interactions.
+
+@RestController
+@RequestMapping("${api.endpoint.base-url}/wizards")
+public class WizardController {
+
+    private final WizardService wizardService;
+
+    private final WizardDtoToWizardConverter wizardDtoToWizardConverter; //WizardDto -> Wizard
+    private final WizardToWizardDtoConverter wizardToWizardDtoConverter; //Wizard -> WizardDto
+
+
+    public WizardController(WizardService wizardService, WizardDtoToWizardConverter wizardDtoToWizardConverter, WizardToWizardDtoConverter wizardToWizardDtoConverter) {
+        this.wizardService = wizardService;
+        this.wizardDtoToWizardConverter = wizardDtoToWizardConverter;
+        this.wizardToWizardDtoConverter = wizardToWizardDtoConverter;
+    }
+
+    //findAll
+    @GetMapping
+    public Result findAllWizards() {
+        List<Wizard> foundWizards = this.wizardService.findAll();
+
+        // Convert foundWizards to a list of WizardDtos.
+        List<WizardDto> wizardDtos = foundWizards.stream()
+                .map(this.wizardToWizardDtoConverter::convert)
+                .collect(Collectors.toList());
+        return new Result(true, StatusCode.SUCCESS, "Find All Success", wizardDtos);
+    }
+
+    //findById
+    @GetMapping("/{wizardId}")
+    public Result findWizardById(@PathVariable Integer wizardId) {
+        Wizard foundWizard = this.wizardService.findById(wizardId);
+        WizardDto wizardDto = this.wizardToWizardDtoConverter.convert(foundWizard);
+        return new Result(true, StatusCode.SUCCESS, "Find One Success", wizardDto);
+    }
+
+    //addWizard
+    @PostMapping
+    public Result addWizard(@Valid @RequestBody WizardDto wizardDto) {
+        Wizard newWizard = this.wizardDtoToWizardConverter.convert(wizardDto);
+        Wizard savedWizard = this.wizardService.save(newWizard);
+        WizardDto savedWizardDto = this.wizardToWizardDtoConverter.convert(savedWizard);
+        return new Result(true, StatusCode.SUCCESS, "Add Success", savedWizardDto);
+    }
+
+    @PutMapping("/{wizardId}")
+    public Result updateWizard(@PathVariable Integer wizardId, @Valid @RequestBody WizardDto wizardDto) {
+        Wizard update = this.wizardDtoToWizardConverter.convert(wizardDto);
+        Wizard updatedWizard = this.wizardService.update(wizardId, update);
+        WizardDto updatedWizardDto = this.wizardToWizardDtoConverter.convert(updatedWizard);
+        return new Result(true, StatusCode.SUCCESS, "Update Success", updatedWizardDto);
+    }
+
+    //deleteWizard
+    @DeleteMapping("/{wizardId}")
+    public Result deleteWizard(@PathVariable Integer wizardId) {
+        this.wizardService.delete(wizardId);
+        return new Result(true, StatusCode.SUCCESS, "Delete Success");
+    }
+
+    //assignArtifact
+
+}
