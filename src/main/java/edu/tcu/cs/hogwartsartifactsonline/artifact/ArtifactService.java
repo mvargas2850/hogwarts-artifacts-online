bef4efe03caaba2deff217240b8355a7f46b82ea
mvargas2850 <m.vargas@tcu.edu>
2024-04-27 00:42:39 -0500

    specifications final

--- src/main/java/edu/tcu/cs/hogwartsartifactsonline/artifact/ArtifactService.java
+++ src/main/java/edu/tcu/cs/hogwartsartifactsonline/artifact/ArtifactService.java
@@ -14,8 +14,10 @@ import io.micrometer.observation.annotation.Observed;
 import jakarta.transaction.Transactional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
+
 import java.util.List;
 import java.util.Map;
 
@@ -96,6 +98,28 @@ public class ArtifactService {
 
     public Page<Artifact> findAll(Pageable pageable) {
         return this.artifactRepository.findAll(pageable);
+    }
+
+    public Page<Artifact> findByCriteria(Map<String, String> searchCriteria, Pageable pageable) {
+        Specification<Artifact> spec = Specification.where(null);
+
+        if (StringUtils.hasLength(searchCriteria.get("id"))) {
+            spec = spec.and(ArtifactSpecs.hasId(searchCriteria.get("id")));
+        }
+
+        if (StringUtils.hasLength(searchCriteria.get("name"))) {
+            spec = spec.and(ArtifactSpecs.containsName(searchCriteria.get("name")));
+        }
+
+        if (StringUtils.hasLength(searchCriteria.get("description"))) {
+            spec = spec.and(ArtifactSpecs.containsDescription(searchCriteria.get("description")));
+        }
+
+        if (StringUtils.hasLength(searchCriteria.get("ownerName"))) {
+            spec = spec.and(ArtifactSpecs.hasOwnerName(searchCriteria.get("ownerName")));
+        }
+
+        return this.artifactRepository.findAll(spec, pageable);
     }
 
 }
