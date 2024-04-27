c2aff73046197442c7e5a70a112395fee1e48dca
mvargas2850 <m.vargas@tcu.edu>
2024-04-26 22:44:18 -0500

    enhance findAllArtifacts with paging and sorting.

--- src/main/java/edu/tcu/cs/hogwartsartifactsonline/artifact/ArtifactController.java
+++ src/main/java/edu/tcu/cs/hogwartsartifactsonline/artifact/ArtifactController.java
@@ -7,6 +7,8 @@ import edu.tcu.cs.hogwartsartifactsonline.artifact.dto.ArtifactDto;
 import edu.tcu.cs.hogwartsartifactsonline.system.Result;
 import edu.tcu.cs.hogwartsartifactsonline.system.StatusCode;
 import jakarta.validation.Valid;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -37,14 +39,12 @@ public class ArtifactController {
     }
 
     @GetMapping
-    public Result findAllArtifacts(){
-        List<Artifact> foundArtifacts = this.artifactService.findAll();
-        //convert foundArtifacts to a list of artifactDtos
-        List<ArtifactDto> artifactDtos = foundArtifacts.stream()
-                .map(this.artifactToArtifactDtoConverter::convert)
-                .collect(Collectors.toList());
-        return new Result(true, StatusCode.SUCCESS, "Find All Success", artifactDtos);
-
+    public Result findAllArtifacts(Pageable pageable) {
+        Page<Artifact> artifactPage = this.artifactService.findAll(pageable);
+        // Convert artifactPage to a page of artifactDtos
+        Page<ArtifactDto> artifactDtoPage = artifactPage
+                .map(this.artifactToArtifactDtoConverter::convert);
+        return new Result(true, StatusCode.SUCCESS, "Find All Success", artifactDtoPage);
     }
 
     @PostMapping
@@ -80,5 +80,7 @@ public class ArtifactController {
         String artifactSummary = this.artifactService.summarize(artifactDtos);
         return new Result(true, StatusCode.SUCCESS, "Summarize Success", artifactSummary);
     }
+
+
 
 }
